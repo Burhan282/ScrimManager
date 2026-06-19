5fa7f857ce1a6b5144b2f5c397c304c3aba31586
Burhan Deniz <561059@student.fontys.nl>
2026-06-19 04:11:45 +0200

    Intergration test uitgebreid dat hij nu ook presentatie laag ook test..

--- ScrimManagerTests/RankCalculateIntegrationTests.cs
+++ ScrimManagerTests/RankCalculateIntegrationTests.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
 using ScrimManagerApplication.Application;
 using ScrimManagerApplication.Application.Models;
 using ScrimManagerApplication.Application.Models.DTOModels;
 using ScrimManagerDataAcces.DataAcces.FakeDataBases;
+using ScrimManagerPresentation.Pages.Presentation.Teams;
 
 namespace ScrimManagerTests
 {
@@ -9,7 +14,7 @@ namespace ScrimManagerTests
     public class RankCalculateIntegrationTests
     {
         [TestMethod]
-        public void AcceptJoinRequest_ShouldRecalculateTeamRank()
+        public async Task AcceptJoinRequest_WithPresentationApplicationAndDataLayer_ShouldRecalculateTeamRank()
         {
             // Arrange
             FakeUserRepository fakeUserRepository = new FakeUserRepository();
@@ -33,23 +38,108 @@ namespace ScrimManagerTests
 
             TeamService teamService = new TeamService(fakeTeamRepository, fakeUserRepository);
 
-            CreateTeamDTO team = new CreateTeamDTO
+            CreateTeamModel createTeamPage = new CreateTeamModel(teamService)
             {
-                Name = "Team Alpha",
-                Teamregion = Region.EU
+                CreateTeamDTO = new CreateTeamDTO
+                {
+                    Name = "Team Alpha",
+                    Teamregion = Region.EU
+                }
             };
+            SetPageContext(createTeamPage, userId: 1);
 
-            teamService.CreateTeam(team, 1);
-            teamService.ApplyToTeam(2, 1);
+            TeamDetailsModel teamDetailsPage = new TeamDetailsModel(teamService);
+            SetPageContext(teamDetailsPage, userId: 2);
+
+            TeamRequestModel teamRequestPage = new TeamRequestModel(teamService);
+            SetPageContext(teamRequestPage, userId: 1);
 
             // Act
-            teamService.AcceptJoinRequest(1, 1);
+            await createTeamPage.OnPostAsync();
+            teamDetailsPage.OnPostApply(1);
+            teamRequestPage.OnPostAccept(1);
 
             // Assert
             Team? updatedTeam = teamService.GetTeamById(1);
 
             Assert.IsNotNull(updatedTeam);
             Assert.AreEqual(Rank.GoldII, updatedTeam.Teamrank);
+        }
+
+        private static void SetPageContext(PageModel pageModel, int userId)
+        {
+            DefaultHttpContext httpContext = new DefaultHttpContext
+            {
+                Session = new TestSession()
+            };
+            httpContext.Session.SetInt32("UserId", userId);
+
+            pageModel.PageContext = new PageContext
+            {
+                HttpContext = httpContext,
+                RouteData = new RouteData()
+            };
+            pageModel.TempData = new TempDataDictionary(httpContext, new TestTempDataProvider());
+        }
+
+        private class TestTempDataProvider : ITempDataProvider
+        {
+            public IDictionary<string, object> LoadTempData(HttpContext context)
+            {
+                return new Dictionary<string, object>();
+            }
+
+            public void SaveTempData(HttpContext context, IDictionary<string, object> values)
+            {
+            }
+        }
+
+        private class TestSession : ISession
+        {
+            private readonly Dictionary<string, byte[]> values = new();
+
+            public IEnumerable<string> Keys => values.Keys;
+
+            public string Id { get; } = Guid.NewGuid().ToString();
+
+            public bool IsAvailable => true;
+
+            public void Clear()
+            {
+                values.Clear();
+            }
+
+            public Task CommitAsync(CancellationToken cancellationToken = default)
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task LoadAsync(CancellationToken cancellationToken = default)
+            {
+                return Task.CompletedTask;
+            }
+
+            public void Remove(string key)
+            {
+                values.Remove(key);
+            }
+
+            public void Set(string key, byte[] value)
+            {
+                values[key] = value;
+            }
+
+            public bool TryGetValue(string key, out byte[] value)
+            {
+                if (values.TryGetValue(key, out byte[]? storedValue))
+                {
+                    value = storedValue;
+                    return true;
+                }
+
+                value = Array.Empty<byte>();
+                return false;
+            }
         }
     }
 }
